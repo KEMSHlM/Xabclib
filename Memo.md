c8c4cc0a9a0088e1c1a7f4c3d69132981a598186
kenshin <koyama.kenshin.748@s.kyushu-u.ac.jp>
2023-04-07 17:28:26 +0900

    temp

---
+++ Memo.md
@@ -0,0 +1,4 @@
+# Memo
+> [Xabclib](http://www.abc-lib.org/Xabclib/Release/Readme-v1.03.pdf)
+
+## 
--- samples_c/BICGSTAB/sampleBICGSTAB.c
+++ samples_c/BICGSTAB/sampleBICGSTAB.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
   double beta0 = 0.0, beta = 0.0;
   double *x, *b, *wk, *wk2, *val, *precond, ratparam[50];
 
-// Read input matrix
+  // Read input matrix
   if( (fp = fopen(FILENAME, "r")) == NULL ){
     fprintf(stdout, "File not found.\n");
     exit(1);
@@ -37,7 +37,9 @@ int main(int argc, char *argv[])
   x = (double *)malloc(n * sizeof(double));
   b = (double *)malloc(n * sizeof(double));
   wk2 = (double *)malloc(n * sizeof(double));
+  // row_ptr
   irp = (int *)malloc((n + 1) * sizeof(int));
+  // col_ind
   icol = (int *)malloc(nnz * sizeof(int));
   val = (double *)malloc(nnz * sizeof(double));
   if( x==NULL || b==NULL || wk2==NULL ||
@@ -48,49 +50,59 @@ int main(int argc, char *argv[])
   for(i = 0; i < nnz; i++) fscanf(fp,"%lf", &val[i]);
   fclose(fp);
 
-// Set solution vecter "x"
+  // Set solution vecter "x"
   for(i = 0; i < n; i++) x[i] = 1.0;
-// Make RHS vecter "b" from A*x
+  // Make RHS vecter "b" from A*x
+  // CRS形式で格納された倍制度非対称(U: Non-symmetric)疎行列-ベクトル積の最適な実行方法を判定する．
   OpenATI_DURMV_11(&n, &nnz, irp, icol, val, x, b);
-// Set initial approximate solution vecter "x_0"
+  // Set initial approximate solution vecter "x_0"
   for(i = 0; i < n; i++) x[i] = 0.0;
 
-// Initialize OpenATLib & Xabclib parameters
+  // Initialize OpenATLib & Xabclib parameters
+  // うーん，このOpenATI_DURMV_11で判定して，OpenATI_INITで値を代入してるのかな？
+  // 規定値で初期化する．
   OpenATI_INIT(iatparam, ratparam, &info);
 
+  // iatparamはソルバーの設定値
+  // おそらく，50-1表記はfortranでいう50番目のパラメータ設定
   iatparam[50-1] = 1; // debug info (0:Off, 1:On)
 
-// Set flag of Auto-tuning for selecting SpMV implementation (0:off 3:on)
+  // Set flag of Auto-tuning for selecting SpMV implementation (0:off 3:on)
   iatparam[9-1]=0;
   iatparam[10-1]=12;
 
-// Set preconditioner type
-// 1:None, 2:Jacobi, 3:SSOR, 4:ILU0D, 5:ILU0, 6:ILUT
+  // Set preconditioner type
+  // 1:None, 2:Jacobi, 3:SSOR, 4:ILU0D, 5:ILU0, 6:ILUT
+  // ILU0D: 0レベルのフィルイン.
   iatparam[25-1]=5;
-// Set preconditioner parameters
+  // Set preconditioner parameters
+  // 前処理行列にJacobi反復法に用いた場合，CG１回の反復に対して何回のJacobi反復を行うか
   iatparam[26-1]=5;
+  // iatparam[25-1]が，4,5の場合に値を捨てるしきい値．
   ratparam[25-1]=1.0E-8;
 
-// Allocate work space for preconditioner
- if( iatparam[25-1]<=4){
-  npre = n;
- } else if( iatparam[25-1]==5){
-  npre = 3*nnz/2 + 2*n + 50;
- } else if( iatparam[25-1]==6){
-  npre = 3*(2*iatparam[26-1]+1)*n/2 + 3*n + 50 + 1;
- } 
+  // Allocate work space for preconditioner
+  // 前処理行列のサイズを決める. 公式ドキュメントに載ってる規定値を使う．
+  if( iatparam[25-1]<=4){
+    npre = n;
+  } else if( iatparam[25-1]==5){
+    npre = 3*nnz/2 + 2*n + 50;
+  } else if( iatparam[25-1]==6){
+    npre = 3*(2*iatparam[26-1]+1)*n/2 + 3*n + 50 + 1;
+  }
   precond = (double *)malloc(npre * sizeof(double));
 
-// Allocate work space for bicgstab
+  // Allocate work space for bicgstab
+  // 公式ドキュメントに載ってる規定値を使う．
   lwk = 9*n+(n-1)/2+1;
   wk = (double *)malloc(lwk * sizeof(double));
   if( wk==NULL || precond==NULL ) exit(1);
 
-// Call Xabclib_BICGSTAB
+  // Call Xabclib_BICGSTAB
   Xabclib_BICGSTAB(&n,&nnz,irp,icol,val,b,x,precond,&npre,iatparam,ratparam,wk,&lwk,&info);
   if( info ) printf("**ERROR** info=%d\n", info);
 
-// Check true rerative residual
+  // Check true relative residual
   for(i = 0; i < n; i++) beta0 += b[i] *  b[i];
   OpenATI_DURMV_11(&n, &nnz, irp, icol, val, x, wk2);
   for(i = 0; i < n; i++) beta += (wk2[i] - b[i]) * (wk2[i] - b[i]);
